0fcab296d01f360726f60ebaeda02ffa9f44df71
unknown <boti.lorant.ferenc.22@ady-nagyatad.lan>
2025-09-18 11:24:03 +0200

    09.18

---
+++ Aaf C#/09.18 OOP/OOP 09.18/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOP_09._18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            konyv k = new konyv("A három testőr", "Alexandre Dumas");
+
+            Console.WriteLine(k.ToString());
+        }
+    }
+}
---
+++ Aaf C#/09.18 OOP/OOP 09.18/konyv.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_09._18
+{
+	internal class konyv
+	{
+
+		public string cim;
+		public string szerzo;
+
+		public konyv(string cim, string szerzo) 
+		{
+			this.cim = cim;
+			this.szerzo = szerzo;
+
+		}
+
+        public override string ToString()
+        {
+            return this.cim + "("+this.szerzo+")";
+        }
+	}
+}
