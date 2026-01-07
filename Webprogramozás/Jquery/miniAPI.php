7703deaa619ce282e47ea0e33270713e2ae7527c
unknown <boti.lorant.ferenc.22@ady-nagyatad.lan>
2026-01-07 09:41:22 +0100

    01.07

--- Webprogramozás/Jquery/miniAPI.php
+++ Webprogramozás/Jquery/miniAPI.php
@@ -1,7 +1,24 @@
 <?php
+
+
     $a=(float)$_GET["a"] ?? 0;
     $b=(float)$_GET["b"] ?? 0;
 
-    echo $a+$b;
+    //echo $a+$b;
 
+    $muvelet = $_GET["muvelet"] ?? "";
+    switch ($muvelet)
+    {
+        case "osszeadas":
+            echo $a+$b;
+            break;
+        case "kivonas":
+            echo $a-$b;
+            break;
+        case "szorzas":
+            echo $a*$b;
+            break;
+        default:
+            echo "Nincs művelet: ".$muvelet;
+    }
 ?>
