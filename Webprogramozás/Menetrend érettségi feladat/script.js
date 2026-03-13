356d2566f770b347ade4f6e54c41c09ea463cab0
Loru <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-03-13 10:35:42 +0100

    03.13

--- Webprogramozás/Menetrend érettségi feladat/script.js
+++ Webprogramozás/Menetrend érettségi feladat/script.js
@@ -23,8 +23,8 @@ function betolt()
 
             while((match = regex.exec(x)) !== null)
             {
-                adatok.push(match.groups)
-                adatok.at(-1)["idoPercben"] = parseInt(adatok.at(-1).ora)*60 + parseInt(adatok.at(-1).perc)
+                adatok.push(match.groups);
+                adatok.at(-1)["idoPercben"] = parseInt(adatok.at(-1).ora)*60 + parseInt(adatok.at(-1).perc);
             }
             
             console.log(adatok)
@@ -67,10 +67,68 @@ function feladat2()
 
 function feladat3()
 {
-    let rendezett = adatok.toSorted((a,b) =>
+
+    let rendezett = adatok.toSorted((a,b) => parseInt(a.allomasId) - parseInt(b.allomasId))
+	.toSorted((a,b) => parseInt(a.vonatId) - parseInt(b.vonatId));
+
+	const varakozasok = [,[]];
+
+	for(let i = 1;i < rendezett.length-1;i+=2)
+	{
+		//de ez még nem
+        if(rendezett[i].vonatId!== rendezett[i+1].vonatId)
+        { 
+		    varakozasok.push([]);
+        } 
+        else
+        {
+            varakozasok[rendezett[i].vonatId].push(rendezett[i+1].idoPercben - rendezett[i].idoPercben);
+        }
+
+	}
+
+    let max = -1
+    let vonatMax = 0;
+    let allomasMax = 0;
+
+    for(let i = 1;i < varakozasok.length;i++)
     {
-        
-    });
+        for(let j = 0;j < varakozasok[i].length;j++)
+        {
+            if(max < varakozasok[i][j])
+            {
+                max = varakozasok[i][j]
+                vonatMax = i;
+                allomasMax = j+1;
+            }
+        }
+    }
+
+    max = Math.max(...varakozasok.flat());
+
+    const szurt = varakozasok.filter(egyVonat =>
+        {
+            let f = egyVonat.filter(egyIdo => egyIdo == max);
+            return f;
+        }
+    );
+
+
+    let szurtSzurt = szurt.filter((e,i) => {
+        if(x.length > 0)
+        {
+            return i;
+       }
+    })
+
+    console.log(szurt)
+
+    document.getElementById("vonatSzama").innerHTML = vonatMax
+    document.getElementById("allomas").innerHTML = allomasMax
+    document.getElementById("varakozasido").innerHTML = max
+
+
+
 }
 
 
