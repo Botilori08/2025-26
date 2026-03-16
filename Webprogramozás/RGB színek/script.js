d840791e5259705aa576e36ed26589d6b371fde4
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-03-16 13:14:58 +0100

    03.16

--- Webprogramozás/RGB színek/script.js
+++ Webprogramozás/RGB színek/script.js
@@ -2,7 +2,7 @@ betolt();
 
 let kepAdatok = [];
 
-function betolt()
+function betolt()//1.feladat
 {   
     fetch("kep.txt")
     .then(x=> x.text())
@@ -12,28 +12,38 @@ function betolt()
 
             const sorok = y.split("\r\n");
 
-            kepAdatok.push([])
 
-            sorok.forEach((e,i) =>
+            sorok.forEach(e =>
                 {
-                    
-                    const adatok = e.split("\s");
+
+                    kepAdatok.push([]);
+
+                    let tablaSor =document.createElement("tr");
+
+
+                    const adatok = e.split(" ");
                     for(let j = 0;j < adatok.length;j+=3)
                     {
                         let pixel = {
                             r:parseInt(adatok[j]),
                             g:parseInt(adatok[j+1]),
                             b:parseInt(adatok[j+2]),
+                            RGB: `RGB(${adatok[j]},${adatok[j+1]},${adatok[j+2]})`
                         }
 
-                        kepAdatok[i].push(pixel)
+                        let cella = document.createElement("td");
+                        cella.style.backgroundColor = pixel.RGB;
+                        pixel["cella"] = cella;
+                        tablaSor.appendChild(cella);
+
+                        kepAdatok[kepAdatok.length-1].push(pixel);
                     }
-                }
-            )
+                    document.querySelector("#kep table").appendChild(tablaSor);
+        });
 
-            console.log(sorok)
+            console.log(sorok);
 
-            console.log(kepAdatok)
+            console.log(kepAdatok);
 
         }
     )
@@ -41,5 +51,17 @@ function betolt()
 
 function feladat2()
 {
+    
+    let sor = parseInt(document.getElementById("sorSzam").value) ?? 1;
+    
+    let oszlop = parseInt(document.getElementById("oszlopSzam").value) ?? 1;
+
+    document.getElementById("feladat2-eredmeny").innerHTML = kepAdatok[sor-1][oszlop-1].RGB;
+
+    kepAdatok[sor-1][oszlop-1].cella.style.backgroundColor = yellow;
+
+
+    
+
 
 }
